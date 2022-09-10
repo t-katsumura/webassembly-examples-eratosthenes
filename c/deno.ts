c8a1a7df21483c111f98c734b5b6bec3fed70b00
t-katsumura <t-katsumura@nri.co.jp>
2022-09-10 14:01:00 +0900

    fix program

---
+++ c/deno.ts
@@ -0,0 +1,18 @@
+// wasm should be compiled as standalone wasm
+// run this script with command
+// $ deno run --allow-read deno.ts -- 10000
+
+const wasmCode = await Deno.readFile("./prime-standalone.wasm");
+const wasmModule = new WebAssembly.Module(wasmCode);
+const wasmInstance = new WebAssembly.Instance(wasmModule);
+const prime = wasmInstance.exports.prime as CallableFunction;
+
+let n = +Deno.args[Deno.args.length - 1];
+
+let startTime = performance.now();
+let maxPrime = prime(n);
+let endTime = performance.now();
+
+console.log("given number =", n);
+console.log("max prime =", maxPrime);
+console.log("duration [ms] =", (endTime - startTime).toFixed(2));
